8f21ae4e0d2c2fac2413c5cb07925ffe6bccf3e9
Hector Mg <hectormgwake@gmail.com>
2016-06-08 12:16:45 -0500

    Finishes behavioral tests for each rule

--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -16,5 +16,9 @@ describe Life do
     it "leaves cell alive if it has exactly 2 or 3 live neighbors" do
       expect(life.evaluate(cell)).to eql(1)
     end
+
+    it "revives cell if it is dead and has exactly 3 living neighbors" do
+      expect(life.evaluate(cell)).to eql(1)
+    end
   end
 end
