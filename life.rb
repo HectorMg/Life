d349c26a1125a3048fa254f56d6b80dddd4c1259
Hector Mg <hectormgwake@gmail.com>
2016-06-08 12:24:43 -0500

    Move to 3D Array implementation

--- life.rb
+++ life.rb
@@ -3,16 +3,21 @@ require 'matrix'
 class Life
   def initialize
     @board = Matrix[
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)],
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)],
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)],
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)],
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)],
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)],
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)],
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)],
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)],
-                    [rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2), rand(2)]
+                    [
+                      [rand(2), rand(2), rand(2)],
+                      [rand(2), rand(2), rand(2)],
+                      [rand(2), rand(2), rand(2)]
+                    ],
+                    [
+                      [rand(2), rand(2), rand(2)],
+                      [rand(2), rand(2), rand(2)],
+                      [rand(2), rand(2), rand(2)]]
+                    ],
+                    [
+                      [rand(2), rand(2), rand(2)],
+                      [rand(2), rand(2), rand(2)],
+                      [rand(2), rand(2), rand(2)]
+                    ]
                       ]
   end
 end
