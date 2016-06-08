4aa33bfa04977c96f2ced8edd268a689871de290
Hector Mg <hectormgwake@gmail.com>
2016-06-08 12:14:50 -0500

    Adds tests for evaluating living cases

--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -8,5 +8,13 @@ describe Life do
     it "kills cell if it has less than 2 live neighbors" do
       expect(life.evaluate(cell)).to eql(0)
     end
+
+    it "kills cell if it has more than 3 live neighbors" do
+      expect(life.evaluate(cell)).to eql(0)
+    end
+
+    it "leaves cell alive if it has exactly 2 or 3 live neighbors" do
+      expect(life.evaluate(cell)).to eql(1)
+    end
   end
 end
